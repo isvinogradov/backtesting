85e7da5e8f76a5c941e260b8d2a0869c73fc2a83
isv-desk <isvinogradov@gmail.com>
2026-08-15 11:32:38 +0300

    f

---
+++ load_binance.py
@@ -0,0 +1,97 @@
+import os
+import shutil
+
+import pandas as pd
+
+YEAR = 2026
+TF_RESOLUTION = "5m"
+FF = "{}USDT-{}-{}-{}.csv"
+
+
+def check_file_exists(ticker: str, ix: int) -> bool:
+    filename = FF.format(ticker[:-4], TF_RESOLUTION, YEAR, str(ix).zfill(2))
+    exists = os.path.isfile(filename)
+    if exists:
+        return True
+    else:
+        print(f"File not found: {filename}")
+        return False
+
+
+def single_file_dl(url: str, filename: str) -> bool:
+    r = requests.get(url, stream=True)
+    if r.ok:
+        print(f"Saving to: {os.path.abspath(filename)}")
+        with open(filename, 'wb') as f:
+            for chunk in r.iter_content(chunk_size=1024 * 8):
+                if chunk:
+                    f.write(chunk)
+                    f.flush()
+                    os.fsync(f.fileno())
+        return True
+    else:  # HTTP status code 4XX/5XX
+        print(f"Download failed: {r.status_code}\n{r.text}")
+        return False
+
+
+def download_and_unzip_files(ticker: str) -> None:
+    binance_url_f = "https://data.binance.vision/data/futures/um/monthly/klines/{t}/{tf}/{t}-{tf}-{yyy}-{mon}.zip"
+    fn_f = "{}USDT-{}-{}-{}.zip"
+    for ii in range(1, 13):
+        zip_fn = fn_f.format(ticker[:-4], TF_RESOLUTION, YEAR, str(ii).zfill(2))
+        dl_res = single_file_dl(
+            binance_url_f.format(
+                t=ticker,
+                tf=TF_RESOLUTION,
+                yyy=YEAR,
+                mon=str(ii).zfill(2),
+            ),
+            zip_fn,
+        )
+        if dl_res:
+            shutil.unpack_archive(zip_fn)
+            os.remove(zip_fn)
+            print(f"Removed: {zip_fn}")
+
+
+def merge_files_into_one(ticker: str) -> None:
+    filenames = [FF.format(ticker[:-4], TF_RESOLUTION, YEAR, str(i).zfill(2))
+                 for i in range(1, 13) if check_file_exists(t, i)]
+    df = pd.concat(
+        map(pd.read_csv, filenames),
+        ignore_index=True,
+    )
+    df.open_time *= (1 / 1000)
+    df.open_time = df.open_time.astype(int)
+
+    new_filename = f"{ticker[:-4].lower()}_data_{YEAR}_{TF_RESOLUTION}.csv"
+    df.to_csv(
+        new_filename,
+        index=False,
+        columns=["open_time", "open", "high", "low", "close", "volume"],
+    )
+    print(f"Saved to: {new_filename}")
+
+    # cleanup
+    for f in filenames:
+        os.remove(f)
+        print(f"Removed: {f}")
+
+
+if __name__ == '__main__':
+    tickers = {
+        Ticker.BTC,
+        Ticker.XAU,
+        # Ticker.ETH,
+        # Ticker.DOGE,
+        # Ticker.BNB,
+        # Ticker.AVAX,
+        # Ticker.SOL,
+        # Ticker.XRP,
+    }
+    for t in tickers:
+        print(f" LOADING {t[:-4]} ".center(80, "*"))
+        download_and_unzip_files(t)
+        merge_files_into_one(t)
+        print(f" {t[:-4]} - DONE ".center(80, "*"))
+        print()
